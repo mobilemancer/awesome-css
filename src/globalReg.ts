c9e3f62fd93f294e7860f8884cfd6b97ef82e611
Andreas Wänqvist <andrkarl@live.com>
2023-12-17 18:55:29 +0100

    feat: skeletons

--- src/globalReg.ts
+++ src/globalReg.ts
@@ -7,3 +7,8 @@ export * from "./2023/css-nesting/css-nesting";
 export * from "./2023/container-queries/container-queries";
 export * from "./2023/has-selector/has-selector";
 export * from "./2023/viewport-units/viewport-units";
+export * from "./2023/scroll-snap/scroll-snap";
+export * from "./2023/color-functions/color-functions";
+export * from "./2023/custom-properties/custom-properties";
+export * from "./2023/flex-gap/flex-gap";
+export * from "./2023/scrollbased-anim/scrollbased-anim";
