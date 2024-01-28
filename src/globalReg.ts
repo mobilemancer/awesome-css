95e434e67fdf3c7f47396523485fd8b31f7512b2
Andreas Wänqvist <andrkarl@gmail.com>
2024-01-28 14:55:44 +0100

    fix: has, dvh & container-queries

--- src/globalReg.ts
+++ src/globalReg.ts
@@ -3,15 +3,16 @@ export * from "./missing-page";
 
 export * from "./components/available-usable";
 
+export * from "./2022/viewport-units/viewport-units";
+export * from "./2022/flex-gap/flex-gap";
+
 export * from "./2023/container-queries/container-queries";
 export * from "./2023/grid/grid-page";
 export * from "./2023/css-nesting/css-nesting";
 export * from "./2023/container-queries/container-queries";
 export * from "./2023/has-selector/has-selector";
-export * from "./2023/viewport-units/viewport-units";
 export * from "./2023/scroll-snap/scroll-snap";
 export * from "./2023/color-functions/color-functions";
 export * from "./2023/custom-properties/custom-properties";
-export * from "./2023/flex-gap/flex-gap";
 export * from "./2023/scrollbased-anim/scrollbased-anim";
 export * from "./2023/sub-grid/sub-grid";
