f9bc7d57a855dcb827abbee1dbd2a13a1e2fd64b
Andreas Wänqvist <andrkarl@live.com>
2024-05-04 11:12:11 +0200

    feat: logo zoom in intro

--- src/pages/intro-page.ts
+++ src/pages/intro-page.ts
@@ -1,1 +1,21 @@
-export class IntroPage { }
+export class IntroPage {
+
+    public logo: HTMLElement;
+
+    attached() {
+        // get this components root element
+        const el = document.getElementsByTagName('intro-page')[0];
+        el.addEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll: (event) => void = (event) => {
+        // Handle the scroll event here
+        const el = document.getElementsByTagName('intro-page')[0];
+        if ((el.scrollHeight - document.body.scrollHeight) === el.scrollTop) {
+            // You're at the bottom of the page
+            this.logo.classList.add('zoom');
+        } else {
+            this.logo.classList.remove('zoom');
+        }
+    }
+}
