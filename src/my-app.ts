b57734d0e23494161640920e2cd1a76955a25af9
Andreas Wänqvist <andrkarl@live.com>
2024-05-12 11:07:57 +0200

    feat: conference presenter slide

--- src/my-app.ts
+++ src/my-app.ts
@@ -1,1 +1,11 @@
-export class MyApp { }
+export class MyApp {
+    public showingPresenterSlide = true;
+    attached() {
+        window.addEventListener('keypress', (e) => {
+            if (e.key === ' ') {
+                // hides the presenter slide
+                this.showingPresenterSlide = false;
+            }
+        });
+    }
+}
